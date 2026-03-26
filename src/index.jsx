2d5e6d38fe00d6252b3f1844478928c8a7d6f52b
emrekavsa <emrekavsa@hotmail.com>
2026-03-27 01:57:15 +0200

    vercel fix

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
 const clientId = "b2438f09286046e7952f9170c182ffaf";
-const redirectUri = "http://127.0.0.1:5173";
+const redirectUri = "window.location.origin";
 const scope = "user-read-private user-read-email playlist-read-private playlist-read-collaborative";
 
 createRoot(document.getElementById("root")).render(
