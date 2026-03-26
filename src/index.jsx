ff5749ba6f81c120e650110e80478eb82ed95dac
emrekavsa <emrekavsa@hotmail.com>
2026-03-27 01:58:45 +0200

    test

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
 const clientId = "b2438f09286046e7952f9170c182ffaf";
-const redirectUri = "window.location.origin";
+const redirectUri = "https://spotify-react-myplaylists.vercel.app";
 const scope = "user-read-private user-read-email playlist-read-private playlist-read-collaborative";
 
 createRoot(document.getElementById("root")).render(
